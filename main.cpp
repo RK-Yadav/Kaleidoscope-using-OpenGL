b2c7c6ab4f0e1129ead0d0acf99793c3bdebd740
TRION-rk <rahulkyadav14@gmail.com>
2019-04-08 05:21:18 +0530

    New Features

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int width=700,height=600;
-int p[3],q[3],m,n,p1,q1;
+int p[7],q[7],m,n,p1,q1;
 float r=0,g=0.8,b=0.2;
 
 void draw(int x,int y)
@@ -74,13 +74,13 @@ void selectcolor(int x,int y)
     else if(s==2 && t==2)
         r=0.5,g=0.5,b=1.0;
     else if(s==3 && t==3)
-        r=0.0,g=1.0,b=0.0;
+        r=0.0,g=0.8,b=0.8;
     else if(s==4 && t==4)
         r=0.2,g=0.8,b=0.0;
     else if(s==5 && t==5)
-        r=0.8,g=0.2,b=0.2;
+        r=0.8,g=0.8,b=0.2;
     else
-        r=1.0,g=0.0,b=0.0;
+        r=1.0,g=0.4,b=0.0;
 
 }
 
@@ -95,10 +95,14 @@ void my_mouse(int x,int y)
            p[0]=-m;q[0]=-n;
            p[1]=m;q[1]=-n;
            p[2]=-m;q[2]=n;
+           p[3]=-n;q[3]=m;
+           p[4]=n;q[4]=-m;
+           p[5]=n;q[5]=m;
+           p[6]=-n;q[6]=-m;
        }
         //r= rand()%2+1;r--;
-        //g= rand()%2+1;g--;
-        //b= rand()%2+1;b--;
+       //g= rand()%2+1;g--;
+       // b= rand()%2+1;b--;
 
     selectcolor(m,n);
 
@@ -106,6 +110,10 @@ void my_mouse(int x,int y)
     Redraw(-m,-n,0);
     Redraw(m,-n,1);
     Redraw(-m,n,2);
+    Redraw(-n,m,3);
+    Redraw(n,-m,4);
+    Redraw(n,m,5);
+    Redraw(-n,-m,6);
     i++;
 
 }
